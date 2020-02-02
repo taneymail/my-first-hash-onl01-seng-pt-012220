7cc4f70fc81b365f5677071be25381d0bcfcf10d
TShipley <taneymail@gmail.com>
2020-02-02 05:20:10 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,10 +23,10 @@ def adding
       }
 end
   
-def adding
-  shipping_manifest["muskets"] = 2 
-  shipping_manifest["gun powder"] = 4
+shipping_manifest["muskets"] = 2
+shipping_manifest["gun powder"] = 4
 
 shipping_manifest
 
+ end
 end
