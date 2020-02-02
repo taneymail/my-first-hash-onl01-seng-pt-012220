ac2637c7a4933945da51281816356f96d65277ac
TShipley <taneymail@gmail.com>
2020-02-02 04:30:22 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,19 +1,14 @@
 def my_hash
-  
-
-{"my_hash" => "My Hash"}
-
+  {"my_hash" => "My Hash"}
 end
 
 def shipping_manifest
   
-
 the_manifest = {
   "whale bone corsets" => 5,
   "porcelain vases" => 2,
   "oil paintings" => 3
 }
-
 end
 
 def retrieval
