b7a7ed5e0dd23adcc70083115dd61ddbd09b8593
TShipley <taneymail@gmail.com>
2020-02-02 04:54:53 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,6 +15,14 @@ def retrieval
   shipping_manifest["oil paintings"]
 end
 
+def adding
+  shipping_manifest = {
+    "whale bone corsets" => 5, 
+    "porcelain vases" => 2, 
+    "oil paintings" => 3
+      }
+  end
+  
 
 
 
