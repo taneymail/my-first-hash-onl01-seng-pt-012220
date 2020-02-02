6e68ce5aa80d044ed20feb10c2a6256362880fc7
TShipley <taneymail@gmail.com>
2020-02-02 04:34:20 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -11,14 +11,8 @@ the_manifest = {
 }
 end
 
-def retrieval = {
-    "porcelain vases" => 2, 
-    "oil paintings" => 3
-                 }
-
-shipping_manifest["oil paintings"]
-  
-
+def retrieval
+  shipping_manifest["oil paintings"]
 end
 
 
